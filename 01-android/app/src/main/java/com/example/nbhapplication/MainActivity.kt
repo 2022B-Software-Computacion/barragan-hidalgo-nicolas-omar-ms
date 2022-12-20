615483b9ab03242aa455aa87acb2ba89063b63ff
NicolasBH9 <nicolas.barragan@epn.edu.ec>
2022-12-20 10:51:14 -0500

    ACiloVida
    ciclo de vida de una actividad

--- 01-android/app/src/main/java/com/example/nbhapplication/MainActivity.kt
+++ 01-android/app/src/main/java/com/example/nbhapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.nbhapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonACicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonACicloVida
+            .setOnClickListener{
+                irActividad((ACicloVida::class.java))
+            }
     }
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this,clase)
+        startActivity(intent)
+    }
+
 }
+
